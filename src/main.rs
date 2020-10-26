2360440ae9c761ef6856f3f9a5f39aa2d8af42b1
Drewol <emildraws@gmail.com>
2020-10-26 23:43:44 +0100

    use websockets

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,42 @@ struct MusicData {
 fn main() {
     let mut discord = discord_rpc_client::Client::new(419493676054609959).unwrap();
     discord.start();
+    let server = TcpListener::bind("127.0.0.1:8975").unwrap();
     println!("Discord started");
     loop {
-        let mut any_set = false;
-        for t in windowtitle::WindowIter::new().titles() {
-            let t = t.split('-').next();
-            if t.is_none() {
-                continue;
-            }
-            let t = t.unwrap();
-            if let Ok(music_data) = serde_json::from_str::<MusicData>(t) {
-                if music_data.playing {
-                    let disc_res =
-                        discord.set_activity(|act: discord_rpc_client::models::Activity| {
-                            act.details(music_data.title)
-                                .state(music_data.artists.join(", "))
-                                .assets(|ass| {
-                                    ass.small_image("playing")
-                                        .small_text("Playing")
-                                        .large_image("def")
-                                        .large_text("Youtube Music")
-                                })
-                        });
-                    if disc_res.is_err() {
-                        println!("Failed to set status.");
+        if let Some(stream) = server.incoming().next() {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            println!("Websocket connected");
+            loop {
+                let t = websocket.read_message().unwrap();
+                println!("Got message");
+                if t.is_close() {
+                    println!("Websocket closed");
+                    break;
+                } else if !t.is_text() {
+                    continue;
+                }
+                if let Ok(music_data) = serde_json::from_str::<MusicData>(t.to_text().unwrap()) {
+                    if music_data.playing {
+                        let disc_res =
+                            discord.set_activity(|act: discord_rpc_client::models::Activity| {
+                                act.details(music_data.title)
+                                    .state(music_data.artists.join(", "))
+                                    .assets(|ass| {
+                                        ass.small_image("playing")
+                                            .small_text("Playing")
+                                            .large_image("def")
+                                            .large_text("Youtube Music")
+                                    })
+                            });
+                        if disc_res.is_err() {
+                            println!("Failed to set status.");
+                        }
+                    } else {
+                        discord.clear_activity();
                     }
-                    any_set = disc_res.is_ok();
                 }
             }
         }
-        if !any_set {
-            discord.clear_activity();
-        }
-        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
